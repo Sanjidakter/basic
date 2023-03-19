f32b2f74dd11db0317c5a34c27e7a5f457edfc07
Sanjida Akter <sanjidakter2304@gmail.com>
2023-03-19 19:41:07 +0600

    second commit

---
+++ resources/views/test.php
@@ -0,0 +1,9 @@
+<?php
+  $world = "Hi Sanjida";
+  echo $world;
+?>
+
+{{$world}} <!-- advantage of blade file we don't need echo and the start and end php>
+
+<! instead of normal is else we will use @if and @endif in blade>
+
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    echo "This is home page";
+});
+
+Route::get('/about', function () {
+  return view('about');
+});
+
+
+
+Route::get('/contactasd-asdf-asdfsad',[ContactController::class,'index'])->name('con');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        
+        $users = User::all(); //eloquoent orm form
+        return view('dashboard',compact('users'));
+    })->name('dashboard');
+});
